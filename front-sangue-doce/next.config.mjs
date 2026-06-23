3248192dca6b91a4baef1e2531bfd048e28e98ab
Jander da Costa Nery <jander.webmaster@gmail.com>
2026-06-23 08:00:19 -0400

    chore(infra): add minio object storage setup

--- front-sangue-doce/next.config.mjs
+++ front-sangue-doce/next.config.mjs
@@ -6,10 +6,17 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    dangerouslyAllowLocalIP: process.env.NODE_ENV !== "production",
     remotePatterns: [
       {
         hostname: "github.com",
         protocol: "https",
+      },
+      {
+        hostname: "localhost",
+        pathname: "/sangue-doce/public/**",
+        port: "9610",
+        protocol: "http",
       },
     ],
   },
